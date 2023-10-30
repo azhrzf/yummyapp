09aee3ccf77d0a0fa427c8bbfcf83c5a56170c38
Azhar Zaidan Fauzi <a.zaidan.f.azf616@gmail.com>
2023-10-30 23:05:11 +0700

    Fix detail 2

--- src/scripts/views/render-restaurant.js
+++ src/scripts/views/render-restaurant.js
@@ -59,11 +59,6 @@ const renderRestaurant = async (filterFavorite) => {
     dataRestaurants.map(async (restaurant) => {
       const {id, name, description, pictureId, city, rating} = restaurant;
       const pictureUrl = 'https://restaurant-api.dicoding.dev/images/medium/';
-
-      const apiDetail = `https://restaurant-api.dicoding.dev/detail/${id}`;
-      const cacheDetail = 'yummyapp-api-detail';
-      await fetchAndCache(apiDetail, cacheDetail);
-
       dataShow.innerHTML += `
             <article tabindex=0>
                 <img src=${pictureUrl}${pictureId} alt=restaurant ${id}_${name}>
@@ -89,6 +84,9 @@ const renderRestaurant = async (filterFavorite) => {
                   </div>
                 </article>
             `;
+      const apiDetail = `https://restaurant-api.dicoding.dev/detail/${id}`;
+      const cacheDetail = 'yummyapp-api-detail';
+      await fetchAndCache(apiDetail, cacheDetail);
     });
     const likeClass = document.querySelectorAll('.like');
     await actButton(likeClass);
